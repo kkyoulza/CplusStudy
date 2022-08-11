0b8329ad560b975b2e73b36aefa41bb6681d25a5
kkyoulza <66288087+kkyoulza@users.noreply.github.com>
2022-08-11 11:56:08 +0900

    Create 7.cpp

---
+++ BaekJoon/1/7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a*b<<endl;
+    cout<<a/b<<endl;
+    cout<<a%b<<endl;
+}
+
+// 모든 사칙연산에 대해 물어보는 문제, input 조건이 자연수이기 때문에 0으로 나눌 걱정은 없었다.
