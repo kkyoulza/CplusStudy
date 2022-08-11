cc2f466a267865ce22beabdf53d7426eaa84eb56
kkyoulza <66288087+kkyoulza@users.noreply.github.com>
2022-08-11 13:58:29 +0900

    Create 15.cpp

---
+++ BaekJoon/1/15.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cout << "         ,r\'\"7" << endl;
+    cout << "r`-_   ,\'  ,/" << endl;
+    cout << " \\. \". L_r\'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+}
+
+// 이런 종류의 문제는 눈이 아프다.. 오타를 잘 찾아내는 것이 생명이다
