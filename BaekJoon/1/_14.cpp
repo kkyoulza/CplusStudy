e1e8f61fee601cc7d0eb0f4c1a518ca59a7762af
kkyoulza <66288087+kkyoulza@users.noreply.github.com>
2022-08-11 12:29:14 +0900

    Create _14.cpp

---
+++ BaekJoon/1/_14.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cout<<"|\\_/|"<<endl;
+    cout<<"|q p|   /}"<<endl;
+    cout<<"( 0 )\"\"\"\\"<<endl;
+    cout<<"|\"^\"`    |"<<endl;
+    cout<<"||_/=\\\\__|"<<endl;
+}
+//따옴표, 큰 따옴표 역시 이스케이스 시퀀스를 이용하여 해야 한다.(_13.cpp)에서도 적용
