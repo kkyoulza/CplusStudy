87583ab39deb529dfee7d1a6d37a086feec71591
kkyoulza <66288087+kkyoulza@users.noreply.github.com>
2022-08-11 12:25:29 +0900

    Create _13.cpp

---
+++ BaekJoon/1/_13.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cout<<"\\    /\\"<<endl;
+    cout<<" )  ( ')"<<endl;
+    cout<<"(  /  )"<<endl;
+    cout<<" \\(__)|"<<endl;
+}
+
+//고양이 그리기 -> 이스케이프 시퀀스를 알고 있느냐 여부를 묻는 문제이다.
