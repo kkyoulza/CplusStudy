31b8317592b7b380648b0f8f0bbb51385bd76b4b
kkyoulza <66288087+kkyoulza@users.noreply.github.com>
2022-08-11 11:40:31 +0900

    Create 1_3.cpp

---
+++ BaekJoon/1_3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b;
+    
+}
+
+// 정수 값 두 개를 입력 받아 더한 값을 출력하는 입/출력을 물어보는 문제.
+
+// 까먹었었다면 값의 이동 흐름을 기억하자. 입력은 왼쪽에서 오른쪽, 출력은 오른쪽에서 왼쪽
