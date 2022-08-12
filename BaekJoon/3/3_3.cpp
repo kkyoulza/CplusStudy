4abbd9287a272a630f58a1442644c274e72293c8
kkyoulza <66288087+kkyoulza@users.noreply.github.com>
2022-08-12 11:17:48 +0900

    Create 3_3.cpp

---
+++ BaekJoon/3/3_3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+  int num;
+  cin>>num;
+  
+  int result = 0;
+  for(int i=1;i<=num;i++){
+    
+    result += i;
+   
+  }
+  
+  cout<<result;
+
+}
+  
+
+// 가장 기본적인 방법(반복문 사용)
