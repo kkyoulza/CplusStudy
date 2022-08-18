2a74f0b3016d6785811243bb956d2ea619ebeb5b
Sprim <66288087+kkyoulza@users.noreply.github.com>
2022-08-18 18:29:57 +0900

    Create 8.cpp

---
+++ BaekJoon/3/8.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int count;
+    cin>>count;
+    
+    for(int i=0;i<count;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<""<<endl;
+    }
+    
+}
+// 오타에 주의할 것!
